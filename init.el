8e6a0cff5d6ed0b92fe8e418f1855bdb35366f5d
Mike Baamonde <mbaamonde@gmail.com>
2015-06-05 18:30:56 -0400

    Add ace-window mode.

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 		      clojure-mode
                       company
 		      darkburn-theme
-		      evil
+		      eviln
 		      evil-leader
 		      helm
 		      helm-git-grep
@@ -290,3 +290,20 @@
 
 ;; Company
 (add-hook 'after-init-hook 'global-company-mode)
+
+;; Ace-window
+(global-set-key (kbd "M-p") 'ace-window)
+
+(setq aw-dispatch-always t)
+
+(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+
+(defvar aw-dispatch-alist
+'((?x aw-delete-window " Ace - Delete Window")
+    (?m aw-swap-window " Ace - Swap Window")
+    (?n aw-flip-window)
+    (?v aw-split-window-vert " Ace - Split Vert Window")
+    (?b aw-split-window-horz " Ace - Split Horz Window")
+    (?i delete-other-windows " Ace - Maximize Window")
+    (?o toggle-maximize-buffer))
+"List of actions for `aw-dispatch-default'.")
