17c9e883bbd4055cd2ced18b92014a8dddcaab67
Mike Baamonde <michael.baamonde@sonian.net>
2015-06-22 09:34:08 -0400

    ace-window tweaks.

--- init.el
+++ init.el
@@ -163,16 +163,21 @@
 
 (global-set-key (kbd "M-c") 'ace-jump-mode)
 
+(setq ace-jump-mode-scope 'window)
+
 (setq aw-dispatch-always t)
 
-(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+(setq aw-keys '(?a ?s ?d ?f ?j ?k ?l))
 
 (defvar aw-dispatch-alist
-  '((?x aw-delete-window )
-    (?m aw-swap-window)
-    (?h aw-split-window-vert)
-    (?v aw-split-window-horz)
-    (?o toggle-maximize-buffer)))
+'((?x aw-delete-window " Ace - Delete Window")
+    (?m aw-swap-window " Ace - Swap Window")
+    ;; The vert/horz distinction is so counterintuitive
+    (?v aw-split-window-horz " Ace - Split Horz Window")
+    (?h aw-split-window-vert " Ace - Split Vert Window")
+    (?i delete-other-windows " Ace - Maximize Window")
+    (?o toggle-maximize-buffer))
+"List of actions for `aw-dispatch-default'.")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Helm
