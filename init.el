141d577fda5e79cd06379e1455ea010e1a3b3890
Mike Baamonde <mike.baamonde@elastic.co>
2016-05-20 09:58:32 -0400

    Use deep sky blue for magit.

--- init.el
+++ init.el
@@ -64,7 +64,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-log-author ((t (:foreground "deep sky blue"))))
+ '(magit-log-author ((t (:foreground "steel blue"))))
  '(magit-log-sha1 ((t (:foreground "deep sky blue"))))
  '(region ((t (:background "dim gray"))))
  '(secondary-selection ((t (:background "#5F5F5F"))))
