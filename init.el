d87e14816f7ee965dd0fbc631fbd52e96aa50773
Mike Baamonde <mike.baamonde@elastic.co>
2016-05-20 10:25:54 -0400

    Pretty up helm-swoop.

--- init.el
+++ init.el
@@ -64,6 +64,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(helm-swoop-target-line-block-face ((t (:background "light steel blue" :foreground "#222222"))))
+ '(helm-swoop-target-line-face ((t (:background "light steel blue" :foreground "#222222"))))
  '(magit-log-author ((t (:foreground "steel blue"))))
  '(magit-log-sha1 ((t (:foreground "deep sky blue"))))
  '(region ((t (:background "dim gray"))))
